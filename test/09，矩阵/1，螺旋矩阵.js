e86d4f96e28fe8aff78698d8e5bfed084328abed
李朝琴 <2236653189@douyait.com>
2021-06-18 13:02:58 +0800

    qq

--- test/09，矩阵/1，螺旋矩阵.js
+++ test/09，矩阵/1，螺旋矩阵.js
@@ -1,31 +1,39 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: LCQ
+ * @Date: 2021-06-18 12:01:12
+ * @LastEditors: LCQ
+ * @LastEditTime: 2021-06-18 13:02:40
+ */
 /* 
 矩阵
   螺旋矩阵
   旋转图像
 */
 
-export default (arr) => {
+export default arr => {
   let map = (arr, r = []) => {
     // 处理每一圈的数据的遍历过程
     for (let i = 0, len = arr.length; i < len; i++) {
       if (i === 0) {
-        r = r.concat(arr[i]) // 拿到第一行数据
+        r = r.concat(arr[i]); // 拿到第一行数据
       } else if (i === len - 1) {
-        r = r.concat(arr[i].reverse()) // 拿到最后一行数据
+        r = r.concat(arr[i].reverse()); // 拿到最后一行数据
       } else {
-        r.push(arr[i].pop()) // 拿到中间行的最后一个数
+        r.push(arr[i].pop()); // 拿到中间行的最后一个数
       }
     }
-    arr.shift() // 清除第一行
-    arr.pop() // 清除最后一行
+    arr.shift(); // 清除第一行
+    arr.pop(); // 清除最后一行
     for (let i = arr.length - 1; i >= 0; i--) {
-      r.push(arr[i].shift()) // 拿到中间行的第一个数
+      r.push(arr[i].shift()); // 拿到中间行的第一个数
     }
     if (arr.length) {
-      return map(arr, r) // 递归
+      return map(arr, r); // 递归 sss
     } else {
-      return r
+      return r;
     }
-  }
-  return map(arr, [])
-}
+  };
+  return map(arr, []);
+};
